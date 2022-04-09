c9cb282becfa7c96eeae8bbff42f7fd9dcaa5b22
Sebastian Guevara <sebastianguevara32569830@gmail.com>
2022-04-09 19:03:09 -0300

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import NavbarAdmin from "./pages/NavbarAdmin";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoute from "./routes/AppRoute";
+import Navbar from "./pages/Navbar";
 const App = () => {
   return (
     <>
       <Router>
-        <NavbarAdmin />
+        <Navbar />
+        {/* <NavbarAdmin /> */}
         <AppRoute />
       </Router>
     </>
