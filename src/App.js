50cce0d5ed8998995c9d8e2424fc924b50aa7c76
leandro <martinezleandro2004@gmail.com>
2022-04-09 18:19:46 -0300

    routes navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
-import HomePage from "./pages/HomePage";
-import FormLogin from "./pages/FormLogin";
-import FormRegister from "./pages/FormRegister";
+// import HomePage from "./pages/HomePage";
+// import FormLogin from "./pages/FormLogin";
+// import FormRegister from "./pages/FormRegister";
 import NavbarAdmin from "./pages/NavbarAdmin";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoute from "./routes/AppRoute";
 const App = () => {
   return (
     <>
-      <NavbarAdmin />
+      <Router>
+        <NavbarAdmin />
+        <AppRoute />
+      </Router>
+      {/* <NavbarAdmin />
       <HomePage />
       <FormLogin />
-      <FormRegister />
+      <FormRegister /> */}
     </>
   );
 };
